b65db34cfc994416703ca2e7ee444e347e703945
Arten Novak <97213086+artnovak@users.noreply.github.com>
2023-11-24 10:43:17 +1000

    Update scrpt.py
    improve window behaviour
    add features:
    -choose folder 
    -add few try/except blocks for catch an errors

--- scrpt.py
+++ scrpt.py
@@ -1,31 +1,38 @@
 import os
-from tkinter import Tk, Entry, Button
-def get_entry_value():
-    global value
-    value = entry.get() # Получение значения из Entry
-    root.destroy()
+from tkinter import Tk, Entry, Button, filedialog, ttk, messagebox
+import time
 
-def change():
-    button['text'] = "fill up absolute path for download folder"
+def choose_directory():
+    messagebox.showinfo('info', 'Choose the directory where you want to sort files')
+    global input_value
+    input_value = filedialog.askdirectory()
 
-root = Tk()
-entry = Entry(root, width=50)
-entry.pack()
-button = Button(
-    root,
-    text="fill up absolute path for download folder",
-    command=get_entry_value,
-    width=40,
-    height=3)
-button.config(command=change())
-button.pack()
-root.mainloop()
-
-
-print(f"Путь до папки: {value}")
-
-os.chdir(rf"{value}")
-
+def quitWin():
+    res = messagebox.askyesno('Ну шо ты так', '?')
+    if res == True:
+        messagebox.showinfo('countdown', 'killing window in 5 seconds')
+        time.sleep(5)
+        quit()
+    elif res == False:
+        pass
+    else:
+        messagebox.showerror('error', 'something went wrong!')
+try:
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text='Choose the directory where you want to sort files').grid(row=0, column=0, sticky="W")
+    ttk.Label(frm, text='').grid(row=1)
+    ttk.Button(frm, text="Quit", command=quitWin).grid(row=2, column=2)
+    ttk.Button(frm,
+        text="choose directory",
+        command=choose_directory).grid(row=2, column=3)
+    root.mainloop()
+    print(f"Путь до папки: {input_value}")
+    os.chdir(rf"{input_value}")
+except:
+    print("something went wrong")
+    quit()
 formats = {
     "docx": ["doc", "csv", "docx", "pdf", "ppt", "xlsx", "txt", "json", "rtf", "msg"],
     "picture": ["png", "jpg", "gif", "bmp", "PNG", "GIF", "JPEG", "jpeg", "JPG", "HEIC"],
@@ -37,7 +44,6 @@ formats = {
     "config": ["conf", "msi", "py"],
     "other": [],
            }
-
 list_files = os.listdir()
 set_files = set()
 for k, v in formats.items():
@@ -45,7 +51,6 @@ for k, v in formats.items():
             os.mkdir(f"{k}")
         except:
             continue
-
 for i in range(len(list_files)):
     try:
         format_file = list_files[i][::-1][:list_files[i][::-1].index(".")]
@@ -56,15 +61,14 @@ for i in range(len(list_files)):
     for k, v in formats.items():
         try:
             if format_file in v:
-                os.replace(fr"{list_files[i]}", rf"{value}\{k}\{list_files[i]}")
+                os.replace(fr"{list_files[i]}", rf"{input_value}\{k}\{list_files[i]}")
         except:
             print("Некоторые файлы могут быть заняты другими программами.")
             continue
-
 other = os.listdir()
 for i in range(len(other)):
      if not os.path.isdir(other[i]):
-         os.replace(fr"{other[i]}", rf"{value}\other\{other[i]}")
+         os.replace(fr"{other[i]}", rf"{input_value}\other\{other[i]}")
 
-print(f"как выглядит папка '{value}' после сортировки: ")
-print(*os.listdir(fr"{value}"), sep="\n")
+print(f"как выглядит папка '{input_value}' после сортировки: ")
+print(*os.listdir(fr"{input_value}"), sep="\n")
